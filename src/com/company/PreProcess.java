254492e8bd1c4f6180e8ad7b6e67117d895e3387
TuringMacLee <turingmac@hotmail.com>
2016-12-23 16:58:10 +0800

    Layout

---
+++ src/com/company/PreProcess.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import javax.swing.*;
+
+/**
+ * Created by TuringMac on 2016/12/23.
+ */
+public class PreProcess {
+    private JPanel Panel;
+    private JTextField textField1;
+    private JButton selectPathButton;
+    private JButton savaDataButton;
+    private JTable table1;
+    private JRadioButton SQLDataRadioButton;
+    private JRadioButton plainTextDataRadioButton;
+    private JRadioButton excelDataRadioButton;
+    private JRadioButton jsonDataRadioButton;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("PreProcess");
+        frame.setContentPane(new PreProcess().Panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
