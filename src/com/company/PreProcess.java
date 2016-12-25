dabab91624c88347c6215546f4d3831b57a5a53b
TuringMacLee <turingmac@hotmail.com>
2016-12-25 11:51:51 +0800

    Edit UI

--- src/com/company/PreProcess.java
+++ src/com/company/PreProcess.java
@@ -10,10 +10,10 @@ import java.awt.event.ActionListener;
  */
 public class PreProcess {
     private JPanel Panel;
-    private JTextField textField1;
+    private JTextField filePath;
     private JButton selectPathButton;
     private JButton savaDataButton;
-    private JTable table1;
+    private JTable displayTable;
     private JRadioButton SQLDataRadioButton;
     private JRadioButton plainTextDataRadioButton;
     private JRadioButton excelDataRadioButton;
@@ -25,11 +25,10 @@ public class PreProcess {
     private JPanel excelPanel;
     private JPanel jsonPanel;
     private JPanel XMLPanel;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
-    private JComboBox comboBox1;
+    private JTextField SQLIp;
+    private JTextField SQLPort;
+    private JTextField SQLTable;
+    private JTextField textSeperator;
     private CardLayout cardLayout;
 
     public PreProcess() {
@@ -38,30 +37,35 @@ public class PreProcess {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 cardLayout.show(optionsPanel, "Card1");
+                selectPathButton.setEnabled(false);
             }
         });
         plainTextDataRadioButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 cardLayout.show(optionsPanel, "Card2");
+                selectPathButton.setEnabled(true);
             }
         });
         excelDataRadioButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 cardLayout.show(optionsPanel, "Card3");
+                selectPathButton.setEnabled(true);
             }
         });
         jsonDataRadioButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 cardLayout.show(optionsPanel, "Card4");
+                selectPathButton.setEnabled(true);
             }
         });
         XMLDataRadioButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 cardLayout.show(optionsPanel, "Card5");
+                selectPathButton.setEnabled(true);
             }
         });
     }
@@ -70,14 +74,16 @@ public class PreProcess {
         JFrame frame = new JFrame("PreProcess");
         frame.setContentPane(new PreProcess().Panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-
-
-
-
+        try {
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            SwingUtilities.updateComponentTreeUI(frame);
+        } catch (Exception e) {
+            System.out.println("Substance Raven Graphite failed to initialize");
+        }
         frame.pack();
         frame.setVisible(true);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        System.out.println(screenSize.width + " " + screenSize.height);
+        frame.setSize(new Dimension((int)(screenSize.width * 0.5),(int)(screenSize.height * 0.5)));
     }
-
 }
