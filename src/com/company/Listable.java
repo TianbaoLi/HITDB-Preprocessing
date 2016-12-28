57d473a62651d30a492f7d4793b13427c3a4b57b
TuringMacLee <turingmac@hotmail.com>
2016-12-28 17:19:11 +0800

    Add listable interface

---
+++ src/com/company/Listable.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by TuringMac on 2016/12/28.
+ */
+public interface Listable {
+    public static final int itemAmount = 200;
+
+    public void tableList();
+}
